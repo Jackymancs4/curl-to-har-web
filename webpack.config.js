91882f354bd1177cfb1db0ee05e3604686c972fe
Giacomo Rossetto <jackyman_cs4@live.it>
2019-07-06 18:16:33 +0200

    Add copy-webpack-plugin dependency
    Ensure to move index.html in dist folder for a clean deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './index.js',
@@ -8,5 +9,11 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  },
+  plugins: [
+    new CopyPlugin([{
+      from: './index.html',
+      to: path.resolve(__dirname, 'dist')
+    }, ]),
+  ],
 };
